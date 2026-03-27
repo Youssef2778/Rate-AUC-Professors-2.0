3039293772f6c7158fb888d53853ffbcbb646cea
newbie-crypt <aliabohammar2@gmail.com>
2026-03-27 15:25:06 +0200

    Implement the 'Hide Password' Mechanism

--- Rate_AUC/mainwindow.cpp
+++ Rate_AUC/mainwindow.cpp
@@ -12,3 +12,13 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_checkBox_stateChanged(int arg1)
+{
+    if (arg1 == 2) {
+        ui->lineEdit_2->setEchoMode(QLineEdit::Password);
+    } else if (arg1 == 0) {
+        ui->lineEdit_2->setEchoMode(QLineEdit::Normal);
+    }
+}
+
