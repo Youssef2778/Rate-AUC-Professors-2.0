65964060071b92a16dec4a1b1ca756d1504a11d8
Newbie-crypt <aliabohammar2@gmail.com>
2026-03-27 15:02:32 +0200

    Delete Rate_AUC/build directory

--- Rate_AUC/build/CMakeFiles/Rate_AUC.dir/Rate_AUC_autogen/EWIEGA46WW/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Rate_AUC.dir/Rate_AUC_autogen/EWIEGA46WW/qrc_resources.cpp.o: \
- /mnt/c/Users/aliab/Desktop/Education/Programming/Projects/Rate_AUC_Professor/Rate_AUC/build/Rate_AUC_autogen/EWIEGA46WW/qrc_resources.cpp \
- /usr/include/stdc-predef.h
--- Rate_AUC/build/Desktop-Debug/CMakeFiles/Rate_AUC.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Rate_AUC.
-# This may be replaced when dependencies are built.
